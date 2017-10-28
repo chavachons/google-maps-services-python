a8fa49eab51dffbd479aa256006175b30d11d8f0
Chavachon Supreeyapond <chavachons@gmail.com>
2017-10-28 23:32:49 +0700

    log

--- place/index.py
+++ place/index.py
@@ -3,11 +3,11 @@ import googlemaps
 import json
 from datetime import datetime
 
-# ARRAY_TYPE = ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','electrician','electronics_store','embassy','fire_station','florist','funeral_home','furniture_store','gas_station','gym','hair_care','hardware_store','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','plumber','police','post_office','real_estate_agency','restaurant','roofing_contractor','rv_park','school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','synagogue','taxi_stand','train_station','transit_station','travel_agency','university','veterinary_care','zoo']
-ARRAY_TYPE = ['fire_station','florist','funeral_home','furniture_store','gas_station','gym','hair_care','hardware_store','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','plumber','police','post_office','real_estate_agency','restaurant','roofing_contractor','rv_park','school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','synagogue','taxi_stand','train_station','transit_station','travel_agency','university','veterinary_care','zoo']
+ARRAY_TYPE = ['accounting','airport','amusement_park','aquarium','art_gallery','atm','bakery','bank','bar','beauty_salon','bicycle_store','book_store','bowling_alley','bus_station','cafe','campground','car_dealer','car_rental','car_repair','car_wash','casino','cemetery','church','city_hall','clothing_store','convenience_store','courthouse','dentist','department_store','doctor','electrician','electronics_store','embassy','fire_station','florist','funeral_home','furniture_store','gas_station','gym','hair_care','hardware_store','hindu_temple','home_goods_store','hospital','insurance_agency','jewelry_store','laundry','lawyer','library','liquor_store','local_government_office','locksmith','lodging','meal_delivery','meal_takeaway','mosque','movie_rental','movie_theater','moving_company','museum','night_club','painter','park','parking','pet_store','pharmacy','physiotherapist','plumber','police','post_office','real_estate_agency','restaurant','roofing_contractor','rv_park','school','shoe_store','shopping_mall','spa','stadium','storage','store','subway_station','synagogue','taxi_stand','train_station','transit_station','travel_agency','university','veterinary_care','zoo']
 
-API_KEY = 'AIzaSyAAKEfRsQq9pukks7umWncxcbPUIRPuEyA'
-LOCATION = (18.7296314, 98.9279261)
+API_KEY = 'AIzaSyApSqzNGQK1GeCSVl5KzsUAjf58AA57fNU'
+# LOCATION = (18.7296314, 98.9279261)
+LOCATION = (19.2705207,99.8750197)
 RADIUS = 50000
 LANGUAG = 'th'
 MIN_PRICE = None
@@ -17,6 +17,7 @@ TYPE = 'department_store'
 gmaps = googlemaps.Client(key=API_KEY)
 
 def searchPlaces(_type=None,_token=None):
+    print('search', _type)
     places_result = gmaps.places('', location=LOCATION,
                         radius=RADIUS, language=LANGUAG,
                         min_price=MIN_PRICE, max_price=MAX_PRICE, 
@@ -46,6 +47,7 @@ db = firebase.database()
 
 def insertData(places):
     for place in places:
+        print(place['id'])
         db.child("places").push(place)
         pass
 
